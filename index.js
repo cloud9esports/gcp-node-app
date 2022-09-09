bbd76cb215108cf5b0f8c1527f0f6bc6c88fd7a6
Zack Jordan <pixelwelders@gmail.com>
2022-09-09 15:20:08 -0500

    Add health endpoint.

--- index.js
+++ index.js
@@ -4,7 +4,11 @@ const app = express();
 
 const num = Math.floor(Math.random() * 1000);
 app.get('/', (req, res) => {
-  return res.send(`Server says: ${num}.`);
+  return res.status(200).send(`Server says: ${num}.`);
+});
+
+app.get('/health', (req, res) => {
+  return res.status(200).send('OK');
 });
 
 app.listen(3031, () => {
